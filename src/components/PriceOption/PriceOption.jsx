7a18455f7d42471961421d31b9a8c115425c162d
Alal Hosen <alalhosen1992620120@gmail.com>
2024-12-21 12:17:11 +0600

    some css change

--- src/components/PriceOption/PriceOption.jsx
+++ src/components/PriceOption/PriceOption.jsx
@@ -7,10 +7,10 @@ const PriceOption = ({ option }) => {
   return (
     <div className="bg-blue-500 rounded-md p-4 text-white">
       <h2 className="text-center">
-        <span className="text-7xl font-extrabold">{price}</span>
+        <span className="text-6xl font-extrabold">{price}</span>
         <span className="text-3xl">/mon</span>
       </h2>
-      <h4 className="text-3xl text-center my-8">{name}</h4>
+      <h4 className="text-4xl text-center my-6">{name}</h4>
       {
         features.map((feature, index)=><Feature key={index} feature={feature}></Feature>)
       }
