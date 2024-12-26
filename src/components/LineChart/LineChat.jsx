4b7f36d9dac3057b5453831457b59f03e282495f
Alal Hosen <alalhosen1992620120@gmail.com>
2024-12-26 22:49:46 +0600

    use line chart data

--- src/components/LineChart/LineChat.jsx
+++ src/components/LineChart/LineChat.jsx
@@ -1,6 +1,19 @@
 import { LineChart, Line } from 'recharts';
 
 const LineChat = () => {
+    const data = [
+        { name: "Alice", marks: 78 },
+        { name: "Bob", marks: 85 },
+        { name: "Charlie", marks: 92 },
+        { name: "David", marks: 74 },
+        { name: "Emma", marks: 88 },
+        { name: "Fiona", marks: 91 },
+        { name: "George", marks: 76 },
+        { name: "Hannah", marks: 82 },
+        { name: "Ian", marks: 89 },
+        { name: "Jane", marks: 95 },
+      ];
+      
     return (
         <div>
             
