5e3b7b9547a406cb852a2f62472c0a34e2974267
Alal Hosen <alalhosen1992620120@gmail.com>
2024-12-28 23:45:48 +0600

    useEffect data fetch

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Phones = () => {
 const [phones, setPhones]=useState([]);
 
-
+useEffect(()=>{
+    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    .then(res=>res.json())
+    .then(data =>setPhones(data.data))
+},[])
 
     return (
         <div>
