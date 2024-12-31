82de728ba32c956ab328ce3bb4719578d78aba5b
Alal Hosen <alalhosen1992620120@gmail.com>
2024-12-31 22:30:04 +0600

    get name & price

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -2,24 +2,31 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Phones = () => {
-const [phones, setPhones]=useState([]);
+  const [phones, setPhones] = useState([]);
 
-useEffect(()=>{
+  useEffect(() => {
     // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     // .then(res=>res.json())
     // .then(data =>setPhones(data.data))
 
-    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-    .then(data=>{
-        const phoneData=data.data.data;
-    });
-},[])
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        const phoneData = data.data.data;
+        const phonesWithFakeData = phoneData.map((phone) => {
+            const obj={
+                name:phone.phone_name,
+                price:parseInt(phone.slug.split('_')[1])
+            }
+        });
+      });
+  }, []);
 
-    return (
-        <div>
-            <h2 className="text-5xl">Phones: {phones.length}</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-5xl">Phones: {phones.length}</h2>
+    </div>
+  );
 };
 
 export default Phones;
