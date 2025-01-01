a23598c888822f7f3a6fa8f794b33e1f4708930b
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-01 23:42:40 +0600

    Tooltip

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
+
 import { useEffect, useState } from "react";
-import { BarChart, Bar, ResponsiveContainer } from "recharts";
+import { BarChart, Bar, XAxis, Tooltip } from "recharts";
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
@@ -29,8 +30,11 @@ const Phones = () => {
   return (
     <div>
       <h2 className="text-5xl">Phones: {phones.length}</h2>
-      <BarChart width={600} height={500} data={phones}>
+      <BarChart width={1200} height={500} data={phones}>
         <Bar dataKey="price" fill="#8884d8" />
+        <XAxis dataKey={name}></XAxis>
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
       </BarChart>
     </div>
   );
