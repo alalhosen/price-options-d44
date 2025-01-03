9b0f48ee4849b3d4d7b3e129a039893cbfdf15c5
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-03 23:22:42 +0600

    set loading spinner

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 import { useEffect, useState } from "react";
-import { BarChart, Bar, XAxis, Tooltip } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import { Audio } from "react-loader-spinner";
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -24,11 +26,23 @@ const Phones = () => {
         });
         console.log(phonesWithFakeData);
         setPhones(phonesWithFakeData);
+        setLoading(false);
       });
   }, []);
 
   return (
     <div>
+   {loading &&
+       <Audio
+       height="80"
+       width="80"
+       radius="9"
+       color="green"
+       ariaLabel="three-dots-loading"
+       wrapperStyle
+       wrapperClass
+     />
+   }
       <h2 className="text-5xl">Phones: {phones.length}</h2>
       <BarChart width={1200} height={500} data={phones}>
         <Bar dataKey="price" fill="#8884d8" />
